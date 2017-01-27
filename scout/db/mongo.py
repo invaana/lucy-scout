d30d015624cf9df609b40ce8dc42023cb17066b4
Ravi RT Merugu <rrmerugu@gmail.com>
2017-01-28 02:34:08 +0530

    mongengine models updated. as the data schema changed

--- scout/db/mongo.py
+++ scout/db/mongo.py
@@ -3,69 +3,35 @@
 
 """
 
-from __future__ import unicode_literals
-
-
-from mongoengine import Document,EmbeddedDocument,StringField,DateTimeField,IntField,\
-    ListField,EmbeddedDocumentField
-# Create your models here.
+from mongoengine import StringField, DateTimeField, ListField, DictField, Document, connect
+import datetime
 
 
 
-class PublicationType(EmbeddedDocument):
-    title = StringField()
 
+class ScrapedData(Document):
+    link = StringField(max_length=500)
+    title = StringField(max_length=300)
+    html = StringField()
+    domain = StringField(max_length=200)
+    image = StringField(max_length=500)
 
-class PublicationKeyword(EmbeddedDocument):
-    title = StringField()
+    # entry date in blog
+    published_date = DateTimeField()
+    published_date_unformated = StringField()
+    date =  DateTimeField(db_field='addDate',default=datetime.datetime.now)
 
-
-class Tag(EmbeddedDocument):
-    title = StringField()
-
-
-class Journal(Document):
-    title = StringField()
-    journal_title = StringField()
-    abstract = StringField()
-    full_text = StringField()
-    pub_year = IntField()
-    pub_date = DateTimeField()
-    pmid = IntField()
-    link = StringField()
-
-    category = StringField(max_length=20,
-                                  default='other',
-                                  choices=(
-                                      ('blog', 'Blog'),
-                                      ('website', 'Website'),
-                                      ('journal', 'Journal'),
-                                      ('book', 'Book'),
-                                      ('doc', 'Documentation'),
-                                      ('other', 'Other')
-                                  ))
-    pub_type = ListField(EmbeddedDocumentField(PublicationType))
-    keywords = ListField(EmbeddedDocumentField(PublicationKeyword))
-    # tags = ListField(ReferenceField(Tag))
-
-    source = StringField()
-    image = StringField()
-    pub_date_unformated = StringField()
-    entry_created_at = DateTimeField()
-    entry_updated_at = DateTimeField()
+    def __unicode__(self):
+        return "%s" %( self.link)
 
     meta = {
-        'index_background': True,
-        'index_drop_dups': True,
         'indexes': [
-            'title',
-            '$title',  # text index
-            '#title',  # hashed index
-            # 'abstract',
-            # '$abstract',  # text index
-            '#abstract',  # hashed index
-
-        ]
+            {'fields': ['-link'], 'unique': True,
+             'sparse': True,  },
+        ],
     }
 
+    def save(self, *args, **kwargs):
+        self.date = datetime.datetime.now()
+        super(ScrapedData, self).save(*args, **kwargs)
 
